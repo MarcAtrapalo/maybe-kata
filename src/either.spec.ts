f3c4a86c28caff83849385b00512662c69d10621
Marc Rodriguez <marc.rodriguez@atrapalo.com>
2018-07-20 16:08:07 +0200

    remove .only in test

--- src/either.spec.ts
+++ src/either.spec.ts
@@ -2,7 +2,7 @@ import {expect} from 'chai';
 import {evalStudent} from './either';
 
 describe('evalStudent', () => {
-    it.only('should return 9.25 for Yagami\'s mythology mark', () => {
+    it('should return 9.25 for Yagami\'s mythology mark', () => {
         expect(evalStudent('Yagami', 'mythology')).to.equal(9.25);
     });
 });
